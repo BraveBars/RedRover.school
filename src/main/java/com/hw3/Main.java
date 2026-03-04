f2073fb5ee6f3f29e1f46788535b267119c97af9
Barsukov Sergey <BraveBars@gmail.com>
2026-03-04 18:15:00 +0300

    feat: added solution to the third task in hw3

--- src/main/java/com/hw3/Main.java
+++ src/main/java/com/hw3/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите первое число - a: ");
+        System.out.print("Введите первое целое число - a: ");
         int a = scan.nextInt();
-        System.out.print("Введите второе число - b: ");
+        System.out.print("Введите второе целое число - b: ");
         int b = scan.nextInt();
 
         if (a == b) {
