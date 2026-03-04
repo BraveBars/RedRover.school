610f312671f85109ceef0de1d1f1fe22dcbb0049
Barsukov Sergey <BraveBars@gmail.com>
2026-03-04 19:45:42 +0300

    homework-5: solve task 4

--- src/main/java/com/hw5/Main.java
+++ src/main/java/com/hw5/Main.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
-        System.out.println("Дан массив - {9, 2, 6, 4, 5, 12, 7, 8, 6}");
+        System.out.println("Дан массив: {9, 2, 6, 4, 5, 12, 7, 8, 6}");
 
-        System.out.print("Нечётные числа в массиве - ");
+        System.out.print("Нечётные числа в массиве: ");
         for (int i : array) {
             if (i % 2 != 0) {
                 System.out.print(i + " ");
             }
         }
 
-        System.out.print("\nЗначения массива больше 5 - ");
+        System.out.print("\nЗначения массива больше 5: ");
         for (int i : array) {
             if (i > 5) {
                 System.out.print(i + " ");
@@ -24,6 +24,6 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             array[i] += 15;
         }
-        System.out.print("\nУвеличенные значения массива на 15 - " + Arrays.toString(array));
+        System.out.print("\nУвеличенные значения массива на 15: " + Arrays.toString(array));
     }
 }
---
+++ src/main/java/com/hw6/Task4.java
@@ -0,0 +1,16 @@
+package com.hw6;
+//Дана строка:
+//String s = “Перестановочный алгоритм быстрого действия”;
+//необходимо вывести все буквы “о” из этой строки.
+//Для указанной строки ответ будет “ооооо” (или в столбик)
+public class Task4 {
+    public static void main(String[] args) {
+        String s = "Перестановочный алгоритм быстрого действия";
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'о'){
+                System.out.print(s.charAt(i));
+            }
+        }
+
+    }
+}
