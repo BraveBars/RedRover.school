9cec55df79506f4a863ddfb347ad2cf2fbcf274f
Barsukov Sergey <BraveBars@gmail.com>
2026-03-04 18:21:54 +0300

    homework-4: solve task 1

---
+++ src/main/java/com/hw4/Task1.java
@@ -0,0 +1,9 @@
+package com.hw4;
+
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
