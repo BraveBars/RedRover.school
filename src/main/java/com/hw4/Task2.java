52c9d1db95ca17b30e9130aea9c112e1a2d14a1c
Barsukov Sergey <BraveBars@gmail.com>
2026-03-04 19:04:37 +0300

    homework-4: solve task 2

---
+++ src/main/java/com/hw4/Task2.java
@@ -0,0 +1,11 @@
+package com.hw4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int num = 5;
+        while (num < 10000){
+            System.out.println(num);
+            num = num * 5;
+        }
+    }
+}
